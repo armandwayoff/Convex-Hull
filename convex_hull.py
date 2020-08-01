8aa3c2ecb4833854a3021045060d330b6181747d
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-01 15:03:03 +0200

    finished version !

--- convex_hull.py
+++ convex_hull.py
@@ -1,5 +1,6 @@
 """
 * Convex Hull *
+Method : Andrew's monotone chain convex hull algorithm
 """
 
 import matplotlib.pyplot as plt
@@ -7,33 +8,38 @@ import networkx as nx
 from random import randint
 
 
-class Vertex:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# Code from https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#Python
+def convex_hull(points):
+    points = sorted(set(points))
+
+    if len(points) <= 1:
+        return points
+
+    def cross(o, a, b):
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+    # Build lower hull
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+
+    # Build upper hull
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+
+    return lower[:-1] + upper[:-1]
 
 
-def cross_product(o, a, b):
-    area = (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x)
-    if area < 0:
-        return -1
-    if area > 0:
-        return 1
-    return 0
-
-
-def lowest_vertex_of(lst):
-    a = [v.y for v in lst]
-    return lst[a.index(min(a))]
-
-
-NUMBER_VERTICES = 10
+NUMBER_VERTICES = 50
 WIDTH = HEIGHT = 100  # dimension of the canvas
 VERTEX_SIZE = 150
 
 vertices = []
-unvisited_vertices = []
-hull = []
 G = nx.Graph()
 
 print("* Convex Hull *")
@@ -42,13 +48,15 @@ print("Number of vertices :", NUMBER_VERTICES,
 
 # creation of the vertices
 for i in range(NUMBER_VERTICES):
-    new_vertex = Vertex(randint(0, WIDTH), randint(0, HEIGHT))
+    new_vertex = (randint(0, WIDTH), randint(0, HEIGHT))
     vertices.append(new_vertex)
-    G.add_node(i, pos=(new_vertex.x, new_vertex.y))
+    G.add_node(i, pos=(new_vertex[0], new_vertex[1]))
 
-anchor = lowest_vertex_of(vertices)
-print(anchor)
+hull = [vertices.index(vertex) for vertex in convex_hull(vertices)]
+for i in range(len(hull) - 1):
+    G.add_edge(hull[i], hull[i + 1])
+G.add_edge(hull[0], hull[-1])
 
 pos = nx.get_node_attributes(G, 'pos')
-nx.draw(G, pos, node_size=VERTEX_SIZE, node_color='orange', with_labels=True)
+nx.draw(G, pos, node_size=VERTEX_SIZE, node_color='blue', width=2)
 plt.show()
