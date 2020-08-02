fe9d7a11e9c18376635c9a4316e261ba046e2e7a
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-02 15:54:19 +0200

    Create convex_hull_function.py

---
+++ convex_hull_function.py
@@ -0,0 +1,25 @@
+# Code from https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#Python
+def convex_hull(points):
+    points = sorted(set(points))
+
+    if len(points) <= 1:
+        return points
+
+    def cross(o, a, b):
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+    # Build lower hull
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+
+    # Build upper hull
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+
+    return lower[:-1] + upper[:-1]
