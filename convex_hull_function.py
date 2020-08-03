57628123fd9df89ef7f94428a0dedd5532435035
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-08-03 18:42:48 +0200

    add comments from the internet page

--- convex_hull_function.py
+++ convex_hull_function.py
@@ -1,14 +1,21 @@
 # Code from https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#Python
 def convex_hull(points):
+    
+    # Sort the points lexicographically (tuples are compared lexicographically).
+    # Remove duplicates to detect the case we have just one unique point.
     points = sorted(set(points))
 
+    # Boring case: no points or a single point, possibly repeated multiple times.
     if len(points) <= 1:
         return points
 
+    # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+    # Returns a positive value, if OAB makes a counter-clockwise turn,
+    # negative for clockwise turn, and zero if the points are collinear.
     def cross(o, a, b):
         return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
 
-    # Build lower hull
+    # Build lower hull 
     lower = []
     for p in points:
         while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
@@ -22,4 +29,6 @@ def convex_hull(points):
             upper.pop()
         upper.append(p)
 
+    # Concatenation of the lower and upper hulls gives the convex hull.
+    # Last point of each list is omitted because it is repeated at the beginning of the other list. 
     return lower[:-1] + upper[:-1]
